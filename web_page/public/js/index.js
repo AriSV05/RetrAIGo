497f60b6933b8ce6fefac0fa76f9b12ff4c696e2
AriSV05 <80506478+AriSV05@users.noreply.github.com>
2024-06-16 01:59:33 -0600

    change on webpage part1

--- web_page/public/js/index.js
+++ web_page/public/js/index.js
@@ -1,124 +1,129 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.container');
-    const infoBtn = document.getElementById('infoBtn');
-    const sendBtn = document.getElementById('sendBtn');
-    const modal = document.getElementById('infoModal');
-    const alertModal = document.getElementById('alertModal');
-    const closeModalButtons = document.querySelectorAll('.close');
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  const infoBtn = document.getElementById("infoBtn");
+  const sendHammi = document.getElementById("sendHammi");
+  const sendManh = document.getElementById("sendManh");
+  const modal = document.getElementById("infoModal");
+  const alertModal = document.getElementById("alertModal");
+  const closeModalButtons = document.querySelectorAll(".close");
+  generatePuzzle();
 
-    sendBtn.disabled = true;
+  infoBtn.onclick = () => {
+    modal.style.display = "block";
+  };
 
-    infoBtn.onclick = () => {
-        modal.style.display = "block";
+  closeModalButtons.forEach((button) => {
+    button.onclick = () => {
+      button.closest(".modal").style.display = "none";
+    };
+  });
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    } else if (event.target == alertModal) {
+      alertModal.style.display = "none";
     }
+  };
 
-    closeModalButtons.forEach(button => {
-        button.onclick = () => {
-            button.closest('.modal').style.display = "none";
-        }
-    });
+  function generatePuzzle() {
+    for (let boardIndex = 0; boardIndex < boards.length; boardIndex++) {
+      oneBoard = boards[boardIndex];
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        } else if (event.target == alertModal) {
-            alertModal.style.display = "none";
+      const puzzleContainer = document.createElement("div");
+      puzzleContainer.className = "puzzle-container";
+      puzzleContainer.id = `${boardIndex}`;
+
+      const radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "puzzle";
+      radio.className = "puzzle-radio";
+      radio.value = boardIndex;
+
+      for (let row = 0; row < oneBoard.length; row++) {
+        for (let col = 0; col < oneBoard[row].length; col++) {
+          const value = oneBoard[row][col];
+          const piece = document.createElement("div");
+          piece.className = "puzzle-piece";
+          if (value === "empty") {
+            piece.classList.add("empty");
+            piece.textContent = "Empty";
+          } else {
+            piece.textContent = value;
+          }
+          puzzleContainer.appendChild(piece);
         }
+        puzzleContainer.appendChild(radio);
+        container.appendChild(puzzleContainer);
+      }
     }
+  }
 
-    function generatePuzzle(emptyRow, emptyCol, index) {
-        const puzzleWrapper = document.createElement('div');
-        puzzleWrapper.className = 'puzzle-wrapper';
+  sendManh.addEventListener("click", () => {
+    const selectedRadio = document.querySelector(
+      'input[name="puzzle"]:checked'
+    );
 
-        const puzzleContainer = document.createElement('div');
-        puzzleContainer.className = 'puzzle-container';
+    if (selectedRadio) {
+      const selectedPuzzle = boards[selectedRadio.value];
 
-        const radio = document.createElement('input');
-        radio.type = 'radio';
-        radio.name = 'puzzle';
-        radio.className = 'puzzle-radio';
-        radio.value = index;
+      const filasFormateadas = selectedPuzzle.map((fila) => `[${fila.join(", ")}]`);
 
-        radio.addEventListener('change', () => {
-            sendBtn.disabled = false;
+      // Unir todas las filas formateadas con comas y rodear con corchetes externos
+      const matrizFormateada = `[${filasFormateadas.join(", ")}]`;
+
+      fetch("http://localhost:8000/playManhattan", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", 
+        },
+        body: JSON.stringify({ initial: matrizFormateada }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
         });
 
-        const puzzle = [];
-        let count = 1;
-
-        for (let row = 0; row < 3; row++) {
-            const puzzleRow = [];
-            for (let col = 0; col < 3; col++) {
-                if (row === emptyRow && col === emptyCol) {
-                    puzzleRow.push(null);
-                } else {
-                    puzzleRow.push(count);
-                    count++;
-                }
-            }
-            puzzle.push(puzzleRow);
-        }
-
-
-        puzzleWrapper.dataset.puzzle = JSON.stringify(puzzle);
-
-
-        for (let row = 0; row < puzzle.length; row++) {
-            for (let col = 0; col < puzzle[row].length; col++) {
-                const value = puzzle[row][col];
-                const piece = document.createElement('div');
-                piece.className = 'puzzle-piece';
-                if (value === null) {
-                    piece.classList.add('empty');
-                    piece.textContent = 'Empty';
-                } else {
-                    piece.textContent = value;
-                }
-                puzzleContainer.appendChild(piece);
-            }
-        }
-
-        puzzleWrapper.appendChild(puzzleContainer);
-        puzzleWrapper.appendChild(radio);
-
-        return puzzleWrapper;
+      //location.href ='../html/goal.html';
+    } else {
+      alertModal.style.display = "block";
     }
+  });
 
-    const emptyPositions = [
-        [0, 0], [0, 1], [0, 2],
-        [1, 0], [1, 1], [1, 2],
-        [2, 0]
-    ];
+  sendHammi.addEventListener("click", () => {
+    const selectedRadio = document.querySelector(
+      'input[name="puzzle"]:checked'
+    );
 
-    emptyPositions.forEach((position, index) => {
-        const [emptyRow, emptyCol] = position;
-        const puzzle = generatePuzzle(emptyRow, emptyCol, index);
-        container.appendChild(puzzle);
-    });
+    if (selectedRadio) {
+      const selectedPuzzle = boards[selectedRadio.value];
 
-    sendBtn.addEventListener('click', () => {
-        const selectedRadio = document.querySelector('input[name="puzzle"]:checked');
-        if (selectedRadio) {
-            const selectedPuzzle = selectedRadio.parentElement.dataset.puzzle;
-            
-            fetch('http://localhost:8000/setGoal', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'  // Tipo de contenido JSON
-                },
-                body: JSON.stringify({ goal: "[[1, 2, null], [3, 4, 5], [6, 7, 8]]" }) // Asegúrate de usar 'null' en lugar de 'empty'
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+      const filasFormateadas = selectedPuzzle.map((fila) => `[${fila.join(", ")}]`);
 
-            //location.href ='../html/goal.html';
-        } else {
-            alertModal.style.display = "block";
-        }
-    });
+      // Unir todas las filas formateadas con comas y rodear con corchetes externos
+      const matrizFormateada = `[${filasFormateadas.join(", ")}]`;
+
+      fetch("http://localhost:8000/playHamming", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", 
+        },
+        body: JSON.stringify({ initial: matrizFormateada }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+
+      //location.href ='../html/goal.html';
+    } else {
+      alertModal.style.display = "block";
+    }
+  });
 });
