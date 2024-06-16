568a932f45600ab562b97ca5d5de131bfb736764
AriSV05 <80506478+AriSV05@users.noreply.github.com>
2024-06-16 01:59:55 -0600

    play hamming and play manhattan working

--- prolog/servidor/http_server.pl
+++ prolog/servidor/http_server.pl
@@ -10,51 +10,39 @@
 
 :- ['../juego/a_star'].
 
-:- http_handler('/setGoal', handle_set_goal, [methods([options,post])]).
-:- http_handler('/playHamming', handle_play_hamming, [method(post)]).
-:- http_handler('/playManhattan', handle_play_manhattan, [method(post)]).
+:- http_handler('/playHamming', handle_play_hamming, [methods([options,post])]).
+:- http_handler('/playManhattan', handle_play_manhattan, [methods([options,post])]).
 :- http_handler('/', home, []).
 
-handle_set_goal(Request) :-
+handle_play_hamming(Request) :-
     cors_enable(Request, [methods([get,post,options])]),
     (   memberchk(method(options), Request)
     ->  format('~n')  % No hay cuerpo en la respuesta para OPTIONS
     ;   memberchk(method(post), Request)
-    ->  http_read_json_dict(Request, Query),  % Procesa POST
-        solve_set_goal(Query, Solution),
+    ->  http_read_json_dict(Request, Query),
+        solve_hamming(Query, Solution),
         reply_json_dict(Solution)
     ).
 
-solve_set_goal(_{goal: Goal}, _{message:Message}) :-
-    atom_to_term(Goal, G, _),
-    set_goal(G),
-    Message = "Goal seated".
-
-solve_set_goal(_, _{accepted: false, answer:0, msg:'Error: failed'}).
-
-handle_play_hamming(Request) :-
-    http_read_json_dict(Request, Query),
-    solve_hamming(Query, Solution),
-    reply_json_dict(Solution).
-
 handle_play_manhattan(Request) :-
-    http_read_json_dict(Request, Query),
-    solve_manhattan(Query, Solution),
-    reply_json_dict(Solution).
+    cors_enable(Request, [methods([get,post,options])]),
+    (   memberchk(method(options), Request)
+    ->  format('~n')  % No hay cuerpo en la respuesta para OPTIONS
+    ;   memberchk(method(post), Request)
+    ->  http_read_json_dict(Request, Query),
+        solve_manhattan(Query, Solution),
+        reply_json_dict(Solution)
+    ).
 
-solve_hamming(_{initial: Initial, goal:Goal}, _{actions: Actions, is_solved: Solve}) :-
-    atom_to_term(Initial, I, _),
-    atom_to_term(Goal, G, _),
-    set_goal(G),   
+solve_hamming(_{initial: Initial}, _{actions: Actions, is_solved: Solve}) :-
+    atom_to_term(Initial, I, _),   
     board_from(I, Id),   
     play_astar(Id, hamming_heuristic, Actions, Solve).
 
 solve_hamming(_, _{accepted: false, answer:0, msg:'Error: failed'}).
 
-solve_manhattan(_{initial: Initial, goal:Goal}, _{actions: Actions, is_solved: Solve}) :-
-    atom_to_term(Initial, I, _),
-    atom_to_term(Goal, G, _),
-    set_goal(G),   
+solve_manhattan(_{initial: Initial}, _{actions: Actions, is_solved: Solve}) :-
+    atom_to_term(Initial, I, _),  
     board_from(I, Id),
     play_astar(Id, manhattan_heuristic, Actions, Solve).
 
@@ -75,5 +63,7 @@ home(_Request) :-
     (current_prolog_flag(argv, [SPort | _]) -> true ; SPort='8000'),
     atom_number(SPort, Port),
     format('*** Serving on port ~d *** ~n', [Port]),
+    Goal = [[1, 2, empty], [3, 4, 5], [6, 7, 8]],
+    set_goal(Goal),
     set_setting_default(http:cors, [*]), % Allows cors for every
     server(Port).
