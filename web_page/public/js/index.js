0dbffe9b854fb375bade40fd305f0b956fc2adf2
AriSV05 <80506478+AriSV05@users.noreply.github.com>
2024-06-16 17:12:23 -0600

    updates on web, reload of the page content after a play button is pressed

--- web_page/public/js/index.js
+++ web_page/public/js/index.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
+  const title = document.getElementById("title");
   const infoBtn = document.getElementById("infoBtn");
   const sendHammi = document.getElementById("sendHammi");
   const sendManh = document.getElementById("sendManh");
   const modal = document.getElementById("infoModal");
   const alertModal = document.getElementById("alertModal");
   const closeModalButtons = document.querySelectorAll(".close");
+  const message = document.getElementById("message");
   generatePuzzle();
 
   infoBtn.onclick = () => {
@@ -67,10 +69,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (selectedRadio) {
       const selectedPuzzle = boards[selectedRadio.value];
 
-      const filasFormateadas = selectedPuzzle.map((fila) => `[${fila.join(", ")}]`);
+      const filasFormateadas = selectedPuzzle.map(
+        (fila) => `[${fila.join(", ")}]`
+      );
       const matrizFormateada = `[${filasFormateadas.join(", ")}]`;
 
-      fetchManhattan(matrizFormateada)
+      fetchManhattan(matrizFormateada);
     } else {
       alertModal.style.display = "block";
     }
@@ -85,13 +89,13 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         body: JSON.stringify({ initial: matrizFormateada }),
       });
-  
+
       const allMovementsJson = await response.json();
-      allMovements = allMovementsJson.actions.map(item => item[0]);
-      const costs = allMovementsJson.actions.map(item => item[2]);
-  
-      fetchBoards(allMovements)
-      
+      allMovements = allMovementsJson.actions.map((item) => item[0]);
+      const costs = allMovementsJson.actions.map((item) => item[2]);
+
+      const allBoards = await fetchBoards(allMovements);
+      reloadPage(costs, allBoards.boards, "Manhattan");
     } catch (error) {
       console.error("Error:", error);
     }
@@ -105,17 +109,17 @@ document.addEventListener("DOMContentLoaded", () => {
     if (selectedRadio) {
       const selectedPuzzle = boards[selectedRadio.value];
 
-      const filasFormateadas = selectedPuzzle.map((fila) => `[${fila.join(", ")}]`);
+      const filasFormateadas = selectedPuzzle.map(
+        (fila) => `[${fila.join(", ")}]`
+      );
       const matrizFormateada = `[${filasFormateadas.join(", ")}]`;
 
-      fetchHamming(matrizFormateada)
-      
+      fetchHamming(matrizFormateada);
     } else {
       alertModal.style.display = "block";
     }
   });
 
-  
   async function fetchHamming(matrizFormateada) {
     try {
       const response = await fetch("http://localhost:8000/playHamming", {
@@ -125,20 +129,21 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         body: JSON.stringify({ initial: matrizFormateada }),
       });
-  
+
       const allMovementsJson = await response.json();
-      allMovements = allMovementsJson.actions.map(item => item[0]);
-      const costs = allMovementsJson.actions.map(item => item[2]);
-  
-      fetchBoards(allMovements)
-      
+      allMovements = allMovementsJson.actions.map((item) => item[0]);
+      const costs = allMovementsJson.actions.map((item) => item[2]);
+
+      const allBoards = await fetchBoards(allMovements);
+
+      reloadPage(costs, allBoards.boards, "Hamming");
     } catch (error) {
       console.error("Error:", error);
     }
   }
 
-  async function fetchBoards(allMovements){
-    try{
+  async function fetchBoards(allMovements) {
+    try {
       const boards = await fetch("http://localhost:8000/showBoards", {
         method: "POST",
         headers: {
@@ -146,12 +151,64 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         body: JSON.stringify({ allBoards: allMovements }),
       });
-  
-      return allBoardsShow = await boards.json();
-    }
-    catch (error) {
+
+      return (allBoardsShow = await boards.json());
+    } catch (error) {
       console.error("Error:", error);
     }
+  }
 
+  async function reloadPage(cost, allBoards, heuristic) {
+    title.innerText = `Movements with ${heuristic}`;
+    
+    if(heuristic == "Manhattan"){
+      message.innerHTML = "El algoritmo de manhattan mide las distancias en una cuadrícula moviéndose solo horizontal y verticalmente."
+    }
+    else{
+      message.innerHTML = "El algoritmo de hamming mide las distancias contando cuantos elementos hay fuera de lugar en la secuencia."
+    }
+
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+
+    sendHammi.remove();
+    sendManh.remove();
+
+    if (allBoards.length > 0) {
+      for (let boardIndex = 0; boardIndex < allBoards.length; boardIndex++) {
+        oneBoard = allBoards[boardIndex];
+
+        const puzzleContainer = document.createElement("div");
+        puzzleContainer.className = "puzzle-container";
+
+        const costText = document.createElement("p");
+        costText.innerText = `Movement cost: ${cost[boardIndex]}`;
+        costText.className = "board-cost";
+
+        for (let row = 0; row < oneBoard.length; row++) {
+          for (let col = 0; col < oneBoard[row].length; col++) {
+            const value = oneBoard[row][col];
+            const piece = document.createElement("div");
+            piece.className = "puzzle-piece";
+            if (value === "empty") {
+              piece.classList.add("empty");
+              piece.textContent = "Empty";
+            } else {
+              piece.textContent = value;
+            }
+            puzzleContainer.appendChild(piece);
+          }
+          puzzleContainer.appendChild(costText);
+          container.appendChild(puzzleContainer);
+        }
+      }
+    } else {
+      const text = document.createElement("p");
+      text.className = "not-result"
+      text.innerText = "No se puede resolver este tablero (￣▽￣)''";
+      container.appendChild(text);
+      message.innerHTML = "Este tablero no se puede resolver ya que los movimientos necesarios para realizarse son impares, debido al algoritmo de counting inversions."
+    }
   }
 });
