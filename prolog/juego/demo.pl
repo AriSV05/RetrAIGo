5c0d35bb693aeacf40a45403613714e001a14357
AriSV05 <80506478+AriSV05@users.noreply.github.com>
2024-06-18 21:10:19 -0600

    New prolog demo

--- prolog/juego/demo.pl
+++ prolog/juego/demo.pl
@@ -1,9 +1,10 @@
 :-[a_star].
 
-test(Id) :-
+test(Id, Actions, Solve) :-
     L = [[1, 2, 5], 
         [3, 7, 4], 
         [6, 8, empty]],
-     
     board_from(L, Id),
-    board_show(Id).
+    board_show(Id),
+    set_goal([[empty, 1, 2], [3, 4, 5], [6, 7, 8]]),   
+    play_astar(Id, hamming_heuristic, Actions, Solve).
